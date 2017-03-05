4a9afd6d92207fd43b9d46b88f741b5395aef730
Ailla <aillagabrielli@gmail.com>
2017-03-05 19:13:10 -0300

    Adicionando

--- workspace/ProjetoPROO/src/Modelo/Produto.java
+++ workspace/ProjetoPROO/src/Modelo/Produto.java
@@ -3,6 +3,8 @@ package Modelo;
 public abstract class Produto {
 	private String nomeProduto;
 	private double valorProduto;
+	private char tamanhoProduto;
+	private String codigoProduto;
 	public String getNomeProduto() {
 		return nomeProduto;
 	}
@@ -14,5 +16,17 @@ public abstract class Produto {
 	}
 	public void setValorProduto(double valorProduto) {
 		this.valorProduto = valorProduto;
+	}
+	public char getTamanhoProduto() {
+		return tamanhoProduto;
+	}
+	public void setTamanhoProduto(char tamanhoProduto) {
+		this.tamanhoProduto = tamanhoProduto;
+	}
+	public String getCodigoProduto() {
+		return codigoProduto;
+	}
+	public void setCodigoProduto(String codigoProduto) {
+		this.codigoProduto = codigoProduto;
 	}
 }
